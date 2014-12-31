343019bb5f694bd4cd275c121e4a7e975cd4bf7b
Raakesh kamal B <raakeshkamal@gmail.com>
2014-12-31 12:48:13 +0530

    user router added

--- clozerr/routes/index.js
+++ clozerr/routes/index.js
@@ -1,8 +1,36 @@
+var express = require('express');
+var router = express.Router();
+var settings = require('./settings');
+/* GET home page. */
+var https = require('https');
 
-/*
- * GET home page.
- */
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' })
+router.get('/login', function(req, res) {
+  res.render('index', { title: 'Express' });
+  var options = {
+  hostname: 'https://graph.facebook.com',
+  port: 443,
+  path: '/debug_token?input_token='+req.query.input_token+'&access_token=643340145745435|nyelclS2lAU75ksOpYtfOLNtwOg',
+  method: 'GET'
 };
+
+var req = https.request(options, function(res) {
+  console.log("Statuscode: ", res.statusCode);
+  console.log("headers: ", res.headers);
+
+  res.on('data', function(d) {
+    process.stdout.write(d);
+  });
+});
+req.end();
+
+req.on('error', function(e) {
+  console.error(e);
+});
+  if(options.is_valid=='true')
+  {
+  	db.collections.find({facebook_user_id:options.user_id});
+  }
+});
+
+module.exports = router;
---
+++ clozerr/routes/settings.js
@@ -0,0 +1,7 @@
+module.exports = {
+	"auth":{
+		"facebook":{
+			"app_token":"643340145745435|nyelclS2lAU75ksOpYtfOLNtwOg"
+		}
+	}
+};
