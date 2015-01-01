16ecb2baca62c83f05cb7eba0aa4758f01314c4e
Raakesh kamal B <raakeshkamal@gmail.com>
2015-01-01 17:36:57 +0530

    -a

--- clozerr/routes/offer.js
+++ clozerr/routes/offer.js
@@ -4,24 +4,36 @@ var router = express.Router();
 
 
 var Offer=mongoose.model('Offer',{
-	offerid: Number
+	//offerid: Number
+	type:String,
+	stamps:String,
+	dateCreated:Date,
+	caption:String,
+	description:String
 });
 
 router.get('/get', function(req, res) {
-	var offerid;
-	if(req.query.offerid) offerid=req.query.offerid;
-  	Offer.findOne({offerid:offerid},function (err,data){
+	var id;
+	if(req.query.id) id=req.query.id;
+
+  	Offer.findOne({_id:id},function (err,data){
 		if(err) console.log(err);
-		if(data) res.send('offerid : '+data.offerid );
+		if(data) res.send('offer:_id : '+data._id );
 		else res.end();
 	})
 });
 
 router.get('/create', function(req, res) {
-	var offerid,usertoken;
-	if(req.query.offerid) offerid=req.query.offerid;
-  	var offer=new Offer({offerid:offerid});
-  	res.send('create request recieved for '+offerid);
+	var type,stamps,dateCreated,caption,description;
+	//if(req.query.offerid) offerid=req.query.offerid;
+	if(req.query.type) type = req.query.type;
+	if(req.query.stamps) stamps = req.query.stamps;
+	if(req.query.dateCreated) dateCreated = req.query.dateCreated;
+	if(req.query.caption) caption = req.query.caption;
+	if(req.query.description) description = req.query.description;
+	
+  	var offer=new Offer({type:type,stamps:stamps,dateCreated:dateCreated,caption:caption,description:description});
+  	res.send('create request recieved for '+JSON.stringify(offer));
   	offer.save(function (err){
   		if(err) console.log(err);
   	})
--- clozerr/routes/vendor.js
+++ clozerr/routes/vendor.js
@@ -3,33 +3,46 @@ var mongoose=require('mongoose');
 var router = express.Router();
 
 var Vendor=mongoose.model('Vendor',{
-	vendorid : Number ,
+	//vendorid : Number ,
 	location : {type:[Number],index:'2dsphere'} ,
-	offers : [Number]
+	offers : [String],
+	image : String,
+	offers_old : [String],
+	fid:String,
+	dateCreated:Date
 });
 
 
 router.get('/create', function (req, res) {
-	var vendorid,lat,lon;
-	if(req.query.vendorid) vendorid=req.query.vendorid;
+	var lat,lon,offers=[],image,offers_old=[],fid,dateCreated=new Date();
+	//if(req.query.vendorid) vendorid=req.query.vendorid;
 	if(req.query.lat) lat=req.query.lat;
 	if(req.query.lon) lon=req.query.lon;
-  	res.send('create request recieved for '+vendorid +' '+lat+' '+lon);
+	if(req.query.image) image=req.query.image;
+	if(req.query.fid) fid=req.query.fid;
+
+  
   	var vendor=new Vendor({
-		vendorid:vendorid,
-		location:[lat,lon]
+		//vendorid:vendorid,
+		location:[lat,lon],
+		offers:offers,
+		image:image,
+		offers_old:offers_old,
+		fid:fid,
+		dateCreated:dateCreated
 	});
+	res.send('create request received : <br>'+JSON.stringify(vendor));
 	vendor.save(function (err){
 		if(err) console.log(err);
 	})
 });
 
 router.get('/get', function (req,res){
-	var vendorid;
-	if(req.query.vendorid) vendorid=req.query.vendorid;
-	Vendor.findOne({vendorid:vendorid},function (err,data){
+	var id;
+	if(req.query.id) id=req.query.id;
+	Vendor.findOne({_id:id},function (err,data){
 		if(err) console.log(err);
-		if(data) res.send('vendorid : '+data.vendorid +'<br/>location : '+data.location+'</br>offers list : '+data.offers);
+		if(data) res.send(JSON.stringify(data));
 		else res.end();
 	})
 })
@@ -62,7 +75,7 @@ router.get('/getnear',function (req,res){
 		else{
 			for(var i=0;i<data.length;i++){
 				var vendor=data[i];
-				res.write('vendorid : '+vendor.vendorid +' location : '+vendor.location+' offers list : ['+vendor.offers+']<br/>');
+				res.write(JSON.stringify(vendor);
 			}
 			res.end();
 		}
