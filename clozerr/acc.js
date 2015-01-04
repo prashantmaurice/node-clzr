c518113f9b57f24c26010fd601b7e9d693ed1fc7
Raakesh kamal B <raakeshkamal@gmail.com>
2015-01-04 21:39:35 +0530

    change_of_database_completed_successfully

--- clozerr/acc.js
+++ clozerr/acc.js
@@ -11,16 +11,19 @@ var db=mongoose.connection;
 db.open('mongodb://'+settings.db.mongo.host+'/fin');
 var data=fs.readFileSync('/home/raakesh/Documents/Clozerr data/Account.json' );
 var arr = JSON.parse(data.toString()).results;
-//console.log(arr.length);
+var dat=fs.readFileSync('/home/raakesh/Documents/Clozerr data/Vendor.json' );
+var ary = JSON.parse(data.toString()).results;
+for(var i=0;i<ary.length;i++)
+{
+	
+}
 var user={};
 var k=0;
 for(var i=0;i<arr.length;i++)
 {   
 	if(arr[i].associatedFlows&&arr[i].associatedFlows.length==2&&arr[i].role=='User')
 	user[k++]=arr[i];
-	//console.log(arr);
 }
-//console.log(user);
 var vendor={};
 var v=0;
 for(var i=0;i<arr.length;i++)
@@ -28,14 +31,25 @@ for(var i=0;i<arr.length;i++)
 	if(arr[i].associatedFlows&&arr[i].associatedFlows.length==1&&arr[i].role=='Vendor')
 	vendor[v++]=arr[i];
 	}
-//console.log(vendor);
 for(var i=0;i<k;i++)
 {
   var nuser = new User({
-  	type:"facebook",
+  	type:"User",
+  	auth_type:"facebook",
   	social_id:user[i].profile.fb_socialId,
   	username:user[i].profile.first_name
   });
-  debugger;
   nuser.save();
+}
+for(var i=0;i<v;i++)
+{   
+	var salt = bcrypt.genSaltSync(10);
+    var hash = bcrypt.hashSync( settings.auth.password.default, salt );
+	var nuser = new User({
+     type:"Vendor",
+     auth_type:"password",
+     username:vendor[i].username,
+     password:hash,
+     Vendor_id:
+	});
 }
