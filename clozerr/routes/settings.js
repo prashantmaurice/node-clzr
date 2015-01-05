72ec021da59580d15f6548e178b4790bf9b0be00
SaipraveenB <cs13b025@smail.iitm.ac.in>
2015-01-06 01:01:06 +0530

    added dashboard bg fix

--- clozerr/routes/settings.js
+++ clozerr/routes/settings.js
@@ -13,4 +13,10 @@ module.exports = {
 	"db":{
 		"mongo":{ "host":"db.clozerr.com", "port":"27107", "name":"test" }
 	},
+	"s3":{
+		"access_key":"AKIAJHO66S6AWOPIT2GA",
+		"secret_key":"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",
+		"bucket":"clozerr",
+		"base_path":"app/coupons-alpha"
+	}
 };
