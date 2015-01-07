edd6027d4f0d0b9e35dc93e971cf07303a31cc00
SaipraveenB <cs13b025@smail.iitm.ac.in>
2015-01-08 04:22:28 +0530

    small bug fix

--- clozerr/public/s1.js
+++ clozerr/public/s1.js
@@ -21,16 +21,4 @@ var ctype_s1 = function( $rootScope, $scope, $http ){
     TODO: Register for SocketIO messages here.
   */
 
-  $scope.reg_socket = function() {
-//TODO : will modify this
-    var socket = io();
-    socket.on('signal',function(signal) {
-      console.log( "Signal received: " + signal );
-      if( JSON.parse(signal).vendor_id == $rootScope.user.vendor_id )
-        $scope.update();
-    });
-  }
-
-  $scope.reg_socket();
-
 }
