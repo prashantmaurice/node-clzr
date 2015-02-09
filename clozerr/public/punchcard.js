04f87b32eaad8d86db16a048a5ad906b85f9725f
aravind <arvindsuresh2009@gmail.com>
2015-02-09 15:57:30 +0530

    working_on_punch1

--- clozerr/public/punchcard.js
+++ clozerr/public/punchcard.js
@@ -3,13 +3,15 @@ var punchCardType = function($scope, $rootScope, $http) {
 
 	$scope.offers = [];
 	$scope.punchcardtype = 'pp';
-	$scope.textInputVis = false;
+	$scope.textInfoVis = [];
+
 	$scope.showPunchCardInputs = function() {
 		console.log("working");
 		if($scope.punchcardtype=='pp') {
 			$scope.num = $('#customPunchCountp').val();
 			for(var i=1;i<=$scope.num;i++) {
 				$scope.offers.push(5*i + " % OFF");
+				$scope.textInfoVis.push(true);
 			}
 		}
 		else if($scope.punchcardtype=='px') {
@@ -17,11 +19,20 @@ var punchCardType = function($scope, $rootScope, $http) {
 			console.log($scope.num);
 			for(var i=1;i<=$scope.num;i++) {
 				$scope.offers.push("");
+				$scope.textInfoVis.push(true);
 			}
 		}
 		$scope.textInputVis = true;
 	}
 	$scope.savePunchCardInputs = function() {
 		console.log("saving offers");
+	}
+	$scope.toggleEditSaveOffer = function(offer, $index) {
+		if($scope.textInfoVis[$index]) {
+			$scope.textInfoVis[$index] = false;
+		}
+		else {
+			$scope.textInfoVis[$index] = true;
+		}
 	}
 }
