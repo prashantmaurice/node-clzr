a2a4583ce8930f162240e8fc2c6397e9b8edf4f3
aravind <arvindsuresh2009@gmail.com>
2015-02-11 20:17:56 +0530

    birthday frontend done

--- clozerr/public/birthday.js
+++ clozerr/public/birthday.js
@@ -1,5 +1,8 @@
 var birthdayCtrl = function($scope, $rootScope, $http) {
-	$scope.flipBack = function() {
-		
-	}
+	$scope.flipped = false;
+	$('#notify1st').bootstrapSwitch();
+	$('#notifyExact').bootstrapSwitch();
+	$('#checkBirthdayPref').bootstrapSwitch();
+
+	$("#checkBirthdayPref").on("switch-change", function (e, data) { console.log("change", data.value); });
 }
