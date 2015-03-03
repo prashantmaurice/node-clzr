6ceab9f422277bd717b218733721b9beb4c74943
vignesh-m <vicky.mano@gmail.com>
2015-03-03 13:52:13 +0530

    basic chart layout ready

--- clozerr/public/vendor_charts.js
+++ clozerr/public/vendor_charts.js
@@ -1,3 +1,13 @@
-/**
- * Created by vigneshm on 03/03/15.
- */
+function linechart(array,xkey,ykey,labels,chartname){
+    var d = document.createElement("div");
+    d.id=chartname;
+    d.style.height='250px';
+    var chartdiv=document.getElementById("charts");
+    chartdiv.appendChild(d);
+    var script=document.createElement("script");
+    console.log(array);
+    script.text="new Morris.Line({element: \'"+chartname+"\',data: "+JSON.stringify(array)+" ," +
+    "xkey: \'"+xkey+"\' , ykeys: "+JSON.stringify(ykey)+" , labels: "+JSON.stringify(labels)+"});"
+    console.log(script.text);
+    chartdiv.appendChild(script);
+}
