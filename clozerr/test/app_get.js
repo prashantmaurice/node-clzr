605d82482d10ce1bf8dce3789dfcb46a2321a871
SaipraveenB <cs13b025@smail.iitm.ac.in>
2015-03-04 12:21:58 +0530

    alight changes to get/near tests

--- clozerr/test/app_get.js
+++ clozerr/test/app_get.js
@@ -8,14 +8,15 @@ var assert = require("assert");
 var db = mongoose.connection;
 db.open('mongodb://'+settings.mongo.username+":"+settings.mongo.password+"@"+settings.mongo.host+'/'+settings.mongo.database);
 
+
 describe("Testing get/near functionality", function(){
         console.log("test1");
         it("should not work without lat, long", function(done){
             http.get(settings.core.server + "/vendor/get/near", function(res) {
                 res.on('data', function(body) {
-                    var result = JSON.parse( body.toString() );
+                    var response = JSON.parse( body.toString() );
                     //console.log( result );
-                    result.result.should.equal(false);
+                    response.result.should.equal(false);
                     done();
                 });
             });
@@ -27,12 +28,11 @@ describe("Testing get/near functionality", function(){
             http.get(settings.core.server + "/vendor/get/near?latitude=10&longitude=10", function(res) {
                 res.on('data', function(body) {
                     var result = JSON.parse( body.toString() );
-                    //console.log( result );
-                    //result.result.should.equal(true); 
+                    response.result.should.equal(true);
                     done();
                 });
             });
         });
-
+    
 
 });
