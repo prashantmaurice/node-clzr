77a39541b73384686c48b4ec43cbc55a8a9fdf02
aravind <arvindsuresh2009@gmail.com>
2015-03-16 00:30:59 +0530

    file upload modal fixed

--- clozerr/public/index_profile_editprofile.js
+++ clozerr/public/index_profile_editprofile.js
@@ -2,157 +2,162 @@
 var index_profile_editprofile = function( $rootScope, $scope, $http) {
 	var CLOZERR_API = location.origin + '/';
 	var CLOZERR_VENDORS_URL = CLOZERR_API + "vendor";
-    $scope.myImage='';
-    $scope.myCroppedImage='';
-    var policy,signature;
+  $scope.myImage='';
+  $scope.myCroppedImage='';
+  var policy,signature;
+  
+  $scope.focusShowNothing = function() {
+    console.log("focussed");		
+    $('#statusLocationIndicator').removeClass('fa fa-spinner fa-pulse fa-2x');
+    $('#statusLocationIndicator').removeClass('fa fa-times fa-2x');
+    $('#statusLocationIndicator').removeClass('fa fa-check fa-2x');
+    $('#statusLocationIndicator').addClass('fa fa-spinner fa-pulse fa-2x');
+  }
+
+  $scope.UploadPhoto=function(){
+
     
-	$scope.focusShowNothing = function() {
-		console.log("focussed");		
-		$('#statusLocationIndicator').removeClass('fa fa-spinner fa-pulse fa-2x');
-		$('#statusLocationIndicator').removeClass('fa fa-times fa-2x');
-		$('#statusLocationIndicator').removeClass('fa fa-check fa-2x');
-		$('#statusLocationIndicator').addClass('fa fa-spinner fa-pulse fa-2x');
-	}
+  }
 
-   $scope.UploadPhoto=function(){
+  $scope.getAddress = function(lat, lon) {
+    $http.get( "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + lat + "," + lon).
+    success(function(data, status, headers, config) {
+     console.log(data);
+     $scope.vendorAddressRevGeoCoded = data.results[0].formatted_address;
+   }).error(function(data, status, headers, config) {
 
-   	  
-   		 }
+   });
+ }
+ $scope.$on('page-editprofile',function() {
+  $scope.vendorAddressRevGeoCoded = $scope.getAddress($rootScope.vendor.location[0],$rootScope.vendor.location[1]);
+});	
 
-	$scope.getAddress = function(lat, lon) {
-		$http.get( "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + lat + "," + lon).
-		success(function(data, status, headers, config) {
-			console.log(data);
-			$scope.vendorAddressRevGeoCoded = data.results[0].formatted_address;
-		}).error(function(data, status, headers, config) {
+ $scope.getLocation = function(address) {
+  var geocoder = new google.maps.Geocoder();
+  console.log('blurred');
+  $('#statusLocationIndicator').removeClass('fa fa-spinner fa-pulse fa-2x');
+  geocoder.geocode({ 'address': address }, function (results, status) {
 
-		});
-	}
-	$scope.$on('page-editprofile',function() {
-		$scope.vendorAddressRevGeoCoded = $scope.getAddress($rootScope.vendor.location[0],$rootScope.vendor.location[1]);
-	});	
+   if (status == google.maps.GeocoderStatus.OK) {
+    var latitude = results[0].geometry.location.lat();
+    var longitude = results[0].geometry.location.lng();
+    console.log('latitude : ' + latitude);
+    console.log('longitude : ' + longitude);	
 
-	$scope.getLocation = function(address) {
-		var geocoder = new google.maps.Geocoder();
-		console.log('blurred');
-		$('#statusLocationIndicator').removeClass('fa fa-spinner fa-pulse fa-2x');
-		geocoder.geocode({ 'address': address }, function (results, status) {
+    $scope.vlatitude = latitude;
+    $scope.vlongitude = longitude;
 
-			if (status == google.maps.GeocoderStatus.OK) {
-				var latitude = results[0].geometry.location.lat();
-				var longitude = results[0].geometry.location.lng();
-				console.log('latitude : ' + latitude);
-				console.log('longitude : ' + longitude);	
+    $rootScope.vendor.location[0] = $scope.vlatitude;
+    $rootScope.vendor.location[1] = $scope.vlongitude;
 
-				$scope.vlatitude = latitude;
-				$scope.vlongitude = longitude;
+    $('#statusLocationIndicator').addClass('fa fa-check fa-2x');
+  } else {		
+    $('#statusLocationIndicator').addClass('fa fa-times fa-2x');
+  }
 
-				$rootScope.vendor.location[0] = $scope.vlatitude;
-				$rootScope.vendor.location[1] = $scope.vlongitude;
+});
+}
+var CLOZERR_PASSWORD_URL = CLOZERR_API + "auth/reset/password";
+$scope.wrongData = false;
+$scope.wrongPassword = false;
+$scope.verifyPasswordChange = function(){
+  if( !( $("#new_password_again").val() == $("#new_password").val() ) ){
+   console.log("Password mismatch");
+   $scope.wrongData = true;
+   $scope.wrongPassword = false;
+   return;
+ }
+ else $scope.wrongData = false;
+ $http.get( CLOZERR_API + "auth/login/verifypassword?username=" + $rootScope.user.username + "&password=" + $("#old_password").val() ).
+ success(function(data, status, headers, config) {
 
-				$('#statusLocationIndicator').addClass('fa fa-check fa-2x');
-			} else {		
-				$('#statusLocationIndicator').addClass('fa fa-times fa-2x');
-			}
+   if(data.result) {
 
-		});
-	}
-	var CLOZERR_PASSWORD_URL = CLOZERR_API + "auth/reset/password";
-	$scope.wrongData = false;
-	$scope.wrongPassword = false;
-	$scope.verifyPasswordChange = function(){
-		if( !( $("#new_password_again").val() == $("#new_password").val() ) ){
-			console.log("Password mismatch");
-			$scope.wrongData = true;
-			$scope.wrongPassword = false;
-			return;
-		}
-		else $scope.wrongData = false;
-		$http.get( CLOZERR_API + "auth/login/verifypassword?username=" + $rootScope.user.username + "&password=" + $("#old_password").val() ).
-		success(function(data, status, headers, config) {
+    $scope.wrongPassword = false;
+    $http.get( CLOZERR_PASSWORD_URL + "?access_token=" + localStorage.token + "&new_password=" + $("#new_password").val() ).
+    success(function(data, status, headers, config) {
+     $scope.wrongData = false;
 
-			if(data.result) {
+     $("#new_password_again").val('');
+     $("#new_password").val('');
+     $("#old_password").val('');
+     $('#modalChangePassword').modal('hide');
 
-				$scope.wrongPassword = false;
-				$http.get( CLOZERR_PASSWORD_URL + "?access_token=" + localStorage.token + "&new_password=" + $("#new_password").val() ).
-				success(function(data, status, headers, config) {
-					$scope.wrongData = false;
+     $scope.getDetails();
+   }).error(function(data, status, headers, config) {
 
-					$("#new_password_again").val('');
-					$("#new_password").val('');
-					$("#old_password").val('');
-					$('#modalChangePassword').modal('hide');
+   });
 
-					$scope.getDetails();
-				}).error(function(data, status, headers, config) {
+ }
+ else $scope.wrongPassword = true;
+ 
+}).error(function(data, status, headers, config) {
 
-				});
+});
+}
 
-			}
-			else $scope.wrongPassword = true;
-			
-		}).error(function(data, status, headers, config) {
+$scope.getDetails = function(){
+  var CLOZERR_PROFILE_URL = CLOZERR_API + "auth/profile";
+  var CLOZERR_VENDOR_URL = CLOZERR_API + "vendor/get";
 
-		});
-	}
+  $http.get(CLOZERR_PROFILE_URL + "?access_token=" + localStorage.token).
+  success(function(data, status, headers, config) {
+   console.log( data );
+   $rootScope.user = data;
 
-	$scope.getDetails = function(){
-		var CLOZERR_PROFILE_URL = CLOZERR_API + "auth/profile";
-		var CLOZERR_VENDOR_URL = CLOZERR_API + "vendor/get";
-
-		$http.get(CLOZERR_PROFILE_URL + "?access_token=" + localStorage.token).
-		success(function(data, status, headers, config) {
-			console.log( data );
-			$rootScope.user = data;
-
-			$http.get(CLOZERR_VENDOR_URL + "?vendor_id=" + $rootScope.user.vendor_id).
-			success(function(data, status, headers, config) {
-				console.log( data );
-				$rootScope.vendor = data;
-			}).error(function(data, status, headers, config) {
+   $http.get(CLOZERR_VENDOR_URL + "?vendor_id=" + $rootScope.user.vendor_id).
+   success(function(data, status, headers, config) {
+    console.log( data );
+    $rootScope.vendor = data;
+  }).error(function(data, status, headers, config) {
         /*
         TODO: Throw error here.
         */
-    });
-		}).error(function(data, status, headers, config) {
+      });
+}).error(function(data, status, headers, config) {
       /*
       TODO: Throw error here.
       */
-  });
-	}
+    });
+}
 }
 var upload=angular.module('clozerr',['ngImgCrop','ngSanitize', 'ngS3upload']);	
- 
-      	function Ctrl($scope, $rootScope) {
-        $scope.myImage='';
-        $scope.myCroppedImage='';
+
+function Ctrl($scope, $rootScope) {
+  $scope.myImage='';
+  $scope.myCroppedImage='';
        /* $scope.files = {
-    		};*/
+       };*/
     	//$scope.options=
-    	$scope.performUpload = false;
-        var handleFileSelect=function(evt) {
-          console.log(evt);
-          var file=evt.currentTarget.files[0];
-          $rootScope.file=file;
+
+      $scope.openPhotoUploadDialog = function() {
+        $('#fileInput').trigger('click');
+      }
+
+      $scope.performUpload = false;
+      var handleFileSelect=function(evt) {
+        console.log(evt);
+        var file=evt.currentTarget.files[0];
+        $rootScope.file=file;
           //$scope.fil=file;
           var reader = new FileReader();
           reader.onload = function (evt) {
             console.log('loaded');
             $scope.$apply(function($scope){
               $scope.myImage=evt.target.result;
-                 console.log(evt.target.result);
-          console.log($scope.myImage.size);
-          console.log($scope.myCroppedImage);
-          $scope.key=''+ + (new Date()).getTime() + '-' + randomString(16);
+              console.log(evt.target.result);
+              console.log($scope.myImage.size);
+              console.log($scope.myCroppedImage);
+              $scope.key=''+ + (new Date()).getTime() + '-' + randomString(16);
           //console.log($scope.fil);
           $rootScope.myCroppedImage=$scope.myCroppedImage;
-            });
+        });
           };
           reader.readAsDataURL(file);
         };
         $scope.$watch('myCroppedImage', function(newValue, oldValue) {
          console.log($scope.myCroppedImage);
-        });
+       });
         $scope.$watch('performUpload',function(newValue,oldValue){
         	console.log($scope.performUpload);
         	if($scope.performUpload==true){
@@ -165,7 +170,7 @@ var upload=angular.module('clozerr',['ngImgCrop','ngSanitize', 'ngS3upload']);
         })
         angular.element(document.querySelector('#fileInput')).on('change',handleFileSelect);
       };
-function upload_image(scope, uri, key, acl, type, accessKey, policy, signature, file) {
+      function upload_image(scope, uri, key, acl, type, accessKey, policy, signature, file) {
       //var deferred = $q.defer();
       scope.attempt = true;
       console.log(file);
@@ -251,5 +256,5 @@ function upload_image(scope, uri, key, acl, type, accessKey, policy, signature, 
       var result = '';
       for (var i = length; i > 0; --i) result += chars[Math.round(Math.random() * (chars.length - 1))];
 
-      return result;
+        return result;
     };
