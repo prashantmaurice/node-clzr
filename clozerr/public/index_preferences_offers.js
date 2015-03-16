4f3c91601dda0076e69f919a2b3407eb3cb4d91c
aravind <arvindsuresh2009@gmail.com>
2015-03-17 02:38:55 +0530

    lot of changes

--- clozerr/public/index_preferences_offers.js
+++ clozerr/public/index_preferences_offers.js
@@ -3,8 +3,47 @@ var punchCardType = function($scope, $rootScope, $http) {
 
 	$scope.textInfoVis = [];
 	$scope.hoverCheck = [];
+	$scope.tempOffer = {};
 
-	
+
+	var CLOZERR_API = location.origin + '/';
+	var CLOZERR_VENDORS_URL = CLOZERR_API + "vendor";
+	var CLOZERR_OFFERS_URL = CLOZERR_API + "offer";
+
+	$scope.removeOffer = function($index) {
+
+
+		$http.get( CLOZERR_OFFERS_URL + "/delete?vendor_id=" + $rootScope.vendor._id + "&offer_id=" + $rootScope.vendor.offers[$index]._id + "&access_token=" + localStorage.token ).
+		success(function(data, status, headers, config) {
+			$rootScope.pageChange("home");
+			$rootScope.pageChange("preferences");
+			$rootScope.vendor.offers.splice($index,1);
+		}).error(function(data, status, headers, config) {
+		});
+	}
+
+	$scope.addOffer = function() {
+
+		$scope.tempOffer.caption = "";
+		$scope.textInfoVis[$rootScope.vendor.offers.length] = false;
+		$scope.tempOffer.description = "";
+
+		$rootScope.vendor.offers.push($scope.tempOffer);
+		$http.get( CLOZERR_OFFERS_URL + "/create?caption=" + $scope.tempOffer.caption + "&description=" + $scope.tempOffer.description + "&access_token=" + localStorage.token ).
+		success(function(data, status, headers, config) {
+			console.log(data);
+
+			$http.get( CLOZERR_VENDORS_URL + "/addoffer?vendor_id=" + $rootScope.vendor._id + "&offer_id=" + data.data._id + "&access_token=" + localStorage.token ).
+			success(function(data, status, headers, config) {
+				console.log(data);
+
+				$rootScope.pageChange("home");
+				$rootScope.pageChange("preferences");
+			}).error(function(data, status, headers, config) {
+			});
+		}).error(function(data, status, headers, config) {
+		});	
+	}	
 
 	$scope.showPunchCardInputs = function() {
 		console.log('true here')
