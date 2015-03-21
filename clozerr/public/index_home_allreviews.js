ff02ffe7b14c555f5b610c9ed48ae1694db4a24a
aravind <arvindsuresh2009@gmail.com>
2015-03-21 22:31:15 +0530

    done

--- clozerr/public/index_home_allreviews.js
+++ clozerr/public/index_home_allreviews.js
@@ -2,6 +2,22 @@
 var index_home_allreviews = function( $rootScope, $scope, $http ){
 	
 	$scope.visibility = false;
+	$scope.getNumber = function(stars) {
+		var avgst=0;
+		for(var kw=0;kw<stars.length;kw++) {
+			avgst = avgst + stars[kw];
+		}
+		if(stars.length!=0) {
+			avgst = avgst/stars.length;
+		}
+		avgst = parseInt(avgst+"");
+		return new Array(avgst);   
+	}
+	$scope.getStatusOfReviewStars = function(stars) {
+		if(stars==undefined || stars==0 || stars=="0") {
+			return "No ratings available";
+		}
+	}
 	console.log('controller home-all-reviews');
 	var PAGE_NAME = "home-all-reviews";
 	$scope.$on("page-" + PAGE_NAME, function(){
