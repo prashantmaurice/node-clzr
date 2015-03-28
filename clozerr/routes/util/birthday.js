cc24003033107ae1272e61ea3df2d20053fa7cf9
aravind <arvindsuresh2009@gmail.com>
2015-03-28 14:46:40 +0530

    birthday.js backend completed

--- clozerr/routes/util/birthday.js
+++ clozerr/routes/util/birthday.js
@@ -7,17 +7,18 @@ var Vendor = Models.Vendor;
 var User = Models.User;
 var _ = require('underscore');
 var currentDate = new Date();
-var birthdayMsg = settings.birthday.birthdayWishMessage;
-var compareDateString = currentDate.getDate() + "" + (currentDate.getMonth());
 
 db.open('mongodb://'+settings.db.mongo.username+":"+settings.db.mongo.password+"@"+settings.db.mongo.host+'/'+settings.db.mongo.name);
 
-Vendor.find({settings.notifyBirthday:true}, function(vendors) {
-	User.find({}, function(allUsers) {
-		var bUsers = _.filter(allUsers, function(userobj) {
-			var userBirthDay = userobj.getDate() + "" + userobj.getMonth();
-			return userBirthDay == compareDateString;
-		});
+Vendor.find({'settings.birthday.activated':true}, function(vendors) {
+	User.find({"profile":{$exists: true} , $where: function() {
+		return ((new Date(this.profile.birthday)).getDate() == currentDate.getDate() && (new Date(this.profile.birthday)).getMonth() == currentDate.getMonth());
+	}
+}, function(bUsers) {
+	console.log('in callback');
+	console.log(vendors);
+	console.log(bUsers);
+	if(vendors) {
 		vendors.forEach(function(currentVendor, posV, arrayV) {
 			bUsers.forEach(function(bUserOne, posU, arrayU) {
 				var posc = bUserOne.stamplist.indexOf(currentVendor.fid);
@@ -26,9 +27,9 @@ Vendor.find({settings.notifyBirthday:true}, function(vendors) {
 					user_list.push(bUserOne.gcm_id);
 				}
 			});
-			push.sendMultiPushNotification(user_list, {birthdayWishMessage:currentVendor.name + " " + birthdayMsg});
+			push.sendMultiPushNotification(user_list, {type:"BIRTHDAY", vendor: currentVendor});
 			user_list = [];			
 		});
-	});
+	}
 });
-
+});
