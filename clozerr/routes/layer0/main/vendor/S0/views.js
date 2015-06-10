2c48714704811fe3e32cda873843e108830006a8
aravind <arvindsuresh2009@gmail.com>
2015-06-10 20:13:07 +0530

    small changes

--- clozerr/routes/layer0/main/vendor/S0/views.js
+++ clozerr/routes/layer0/main/vendor/S0/views.js
@@ -5,12 +5,15 @@ var _ = require("underscore");
 var view_vendor_offers_offers_S0=function(params,user){
 	var deferred = Q.defer();
 	registry.getSharedObject("data_vendor").get(params).then(function(vendor) {
+		debugger;
 		registry.getSharedObject("data_vendor_S0").get(params,vendor).then(function(vendor_offers){
+			debugger;
 			var predicate = registry.getSharedObject("handler_predicate_S0");
 			var plist=[]
 			_.each(vendor_offers.offers,function(element,index,array){
 				plist.push(predicate.get(user,vendor,element))
 			})
+			debugger;
 			Q.all(plist).then(function(predlist){
 				deferred.resolve(
 					_.map(_.filter(vendor_offers.offers,
@@ -103,7 +106,83 @@ var view_vendor_offers_limitedtimeoffers = function(params, user) {
 
 	return deferred.promise;
 }
+function removeDuplicatesRewards(user,vendor_id){
+    var valid = true;
+    if(user.lucky_rewards.length==0)
+        return true;
+    for(var i=0;i<user.lucky_rewards.length;i++){
+        if(user.lucky_rewards[i].id.equals(vendor_id))
+            valid = false;
+        if(i==user.lucky_rewards.length-1)
+            return valid;
+    }
+}
+function removeDuplicatesFailed(user,vendor_id){
+    var valid = true;
+    if(user.failed_instances.length==0)
+        return true;
+    for(var i=0;i<user.failed_instances.length;i++){
+        if(user.failed_instances[i].id.equals(vendor_id))
+            valid = false;
+        if(i==user.failed_instances.length-1)
+            return valid;
+    }
+}
+var view_vendor_lucky_checkin  = function(params,user){
+    var deferred = Q.defer();
+    var vendorObj = registry.getSharedObject("data_vendor");
+    var valid ;
+    debugger;
+    if(!user.lucky_rewards)
+    {
+        user.lucky_rewards = [];
+    }
+    if(!user.failed_instances)
+    {
+        user.failed_instances = [];
+    }
+    vendorObj.get(params).then(function(vendor){
+        if(!vendor.trials)
+            vendor.trials = 0;
+        debugger;
+        if(removeDuplicatesFailed(user,vendor._id))
+            {   debugger;
+                if(removeDuplicatesRewards(user,vendor._id))
+                {
+                    if(vendor.trials%2==0)
+                        {   debugger;
+                            user.lucky_rewards.push({id:vendor._id,time:Date.now()});
+                            vendor.trials++;
+                            user.markModified("lucky_rewards");
+                            user.save();
+                            valid = true;
+                            deferred.resolve(valid);
+                        }
+                        else
+                        {
+                            user.failed_instances.push({id:vendor._id,time:Date.now()});
+                            vendor.trials++;
+                            user.markModified("failed_instances");
+                            user.save();
+                            valid = false;
+                            deferred.resolve(valid);
+                        }
+                        vendor.save();
+                    }
+                    else
+                    {
+                        deferred.resolve(false);
+                    }
+                }
+                else
+                {
+                    deferred.resolve(false);
+                }
+            });
+return deferred.promise;
+}
 
+registry.register("view_vendor_lucky_checkin",{get:view_vendor_lucky_checkin});
 registry.register("view_vendor_offers_checkin_S0", {get:view_vendor_offers_checkin_S0});
 registry.register("view_vendor_offers_validate_S0", {get:view_vendor_offers_validate_S0});
 registry.register("view_vendor_offers_offers_S0", {get:view_vendor_offers_offers_S0});
