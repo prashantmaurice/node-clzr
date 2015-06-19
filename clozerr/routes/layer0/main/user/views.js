13437dea6f5a92589d55ad6d815f4024017313af
raakeshkamal <raakeshkamal@gmail.com>
2015-06-19 21:35:50 +0530

    pinned url added

--- clozerr/routes/layer0/main/user/views.js
+++ clozerr/routes/layer0/main/user/views.js
@@ -3,22 +3,12 @@ var Q=require('q');
 var _=require('underscore');
 var view_user_add_favourites=function(params,user){
 	var deferred=Q.defer();
-	console.log('On favourites add url')
-var vendorObjectM=registry.getSharedObject("data_vendor");
-var userObjectM=registry.getSharedObject("live_session");
-var vendorObj=null;
-if(!user.favourites)
-{
-	user.favourites={};
-	user.favourites.vendor=[];
-	user.favourites.offer=[];
-}
-if(params.vendor_id)
-{   if(!user.favourites.vendor)
-     user.favourites.vendor = [];    
-	if(_.indexOf(user.favourites.vendor,params.vendor_id)==-1)
+	console.log('On favourites add url');
+  if(!user.favourites)
+     user.favourites = [];    
+	if(_.indexOf(user.favourites,params.vendor_id)==-1&&params.vendor_id)
 		{
-		user.favourites.vendor.push(params.vendor_id);
+		user.favourites.push(params.vendor_id);
 		user.markModified("favourites");
 		user.save(function(err,user){
 			deferred.resolve(user);
@@ -30,13 +20,17 @@ if(params.vendor_id)
 	else
 		deferred.resolve(user);
 
+	return deferred.promise;
 }
-else if(params.offer_id){
-	if(!user.favourites.offer)
-		user.favourites.offer=[];
-	if(_.indexOf(user.favourites.offer,params.offer_id)==-1)
-	{	user.favourites.offer.push(params.offer_id);
-		user.markModified("favourites");
+var view_user_add_pinned=function(params,user){
+	var deferred=Q.defer();
+	console.log('On favourites add url');
+
+	if(!user.pinned)
+		user.pinned=[];
+	if(_.indexOf(user.pinned,params.offer_id)==-1&&params.offer_id)
+	{	user.pinned.push(params.offer_id);
+		user.markModified("pinned");
 		user.save(function(err,user){
 		deferred.resolve(user);
 		},
@@ -46,14 +40,10 @@ else if(params.offer_id){
 	}
 	else
 		deferred.resolve(user);
-}
-else
-{ 
- deferred.resolve(user.favourites);
-}
+
 	return deferred.promise;
 }
-
 global.registry.register("view_user_add_favourites",{get:view_user_add_favourites});
+global.registry.register("view_user_add_pinned",{get:view_user_add_pinned});
 
 module.exports={add_favourites:view_user_add_favourites};
