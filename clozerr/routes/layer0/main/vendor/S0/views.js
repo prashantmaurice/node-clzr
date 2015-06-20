2111bd657abf27d911b9183cfeac8c56d75e1669
aravind <arvindsuresh2009@gmail.com>
2015-06-20 17:42:21 +0530

    vendor create url done.

--- clozerr/routes/layer0/main/vendor/S0/views.js
+++ clozerr/routes/layer0/main/vendor/S0/views.js
@@ -229,4 +229,4 @@ registry.register("view_vendor_lucky_checkin",{get:view_vendor_lucky_checkin});
 registry.register("view_vendor_offers_checkin_S0", {get:view_vendor_offers_checkin_S0});
 registry.register("view_vendor_offers_validate_S0", {get:view_vendor_offers_validate_S0});
 registry.register("view_vendor_offers_offers_S0", {get:view_vendor_offers_offers_S0});
-registry.register("view_vendor_offers_limitedtimeoffers", {get:view_vendor_offers_limitedtimeoffers});
+registry.register("view_vendor_offers_limited_time_offers", {get:view_vendor_offers_limitedtimeoffers});
