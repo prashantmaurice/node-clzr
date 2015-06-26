ce0e6966ef4d64fa15de473538055d853b1c4ac6
vignesh-m <vicky.mano@gmail.com>
2015-06-26 15:03:52 +0530

    added fav,pinned remove urls

--- clozerr/routes/layer0/main/user/views.js
+++ clozerr/routes/layer0/main/user/views.js
@@ -22,6 +22,27 @@ var view_user_add_favourites=function(params,user){
 
 	return deferred.promise;
 }
+var view_user_remove_favourites=function(params,user){
+	var deferred=Q.defer();
+	console.log('On favourites remove url');
+  	if(!user.favourites)
+    	user.favourites = [];   
+	if(params.vendor_id) {
+		idx=_.indexOf(user.favourites,params.vendor_id)
+		if(idx!=-1){
+			user.favourites.splice(idx,idx);
+			user.markModified("favourites");
+			user.save(function(err){
+				deferred.reject(err)
+			})
+		}
+		deferred.resolve(user);
+	}
+	else
+		deferred.reject({description:'No vendorid'});
+
+	return deferred.promise;
+}
 var view_user_add_pinned=function(params,user){
 	var deferred=Q.defer();
 	console.log('On favourites add url');
@@ -43,7 +64,30 @@ var view_user_add_pinned=function(params,user){
 
 	return deferred.promise;
 }
+var view_user_remove_pinned=function(params,user){
+	var deferred=Q.defer();
+	console.log('On pinned remove url');
+  	if(!user.pinned)
+    	user.pinned = [];   
+	if(params.offer_id) {
+		idx=_.indexOf(user.pinned,params.offer_id)
+		if(idx!=-1){
+			user.pinned.splice(idx,idx);
+			user.markModified("pinned");
+			user.save(function(err){
+				deferred.reject(err)
+			})
+		}
+		deferred.resolve(user);
+	}
+	else
+		deferred.reject({description:'No offer_id'});
+
+	return deferred.promise;
+}
 global.registry.register("view_user_add_favourites",{get:view_user_add_favourites});
 global.registry.register("view_user_add_pinned",{get:view_user_add_pinned});
+global.registry.register("view_user_remove_favourites",{get:view_user_remove_favourites});
+global.registry.register("view_user_remove_pinned",{get:view_user_remove_pinned});
 
 module.exports={add_favourites:view_user_add_favourites};
