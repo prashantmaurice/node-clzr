98c16115f757fe7b5f8821b7e149c90bad4dd6fa
vignesh-m <vicky.mano@gmail.com>
2015-06-30 17:18:41 +0530

    analytics/hit records data

--- clozerr/routes/layer0/main/analytics/views.js
+++ clozerr/routes/layer0/main/analytics/views.js
@@ -1,11 +1,13 @@
 var registry=global.registry
 var Q = require('q')
 var view_analytics_hit=function(params,user){
-	create_params={
-		timeStamp:params.time || Date.now(),
-		user:user.id,
-		metric:params.metric,
-		dimensions:params.dimensions
-	}
+	var Analytics = registry.getSharedObject('models_Analytics')
+	var analytics_obj=new Analytics();
+	console.log(params)
+	analytics_obj.timeStamp=params.time || Date.now();
+	analytics_obj.user=user.id;
+	analytics_obj.metric=params.metric;
+	analytics_obj.dimensions=params.dimensions;
+	return Q(analytics_obj.save())
 }
 registry.register("view_analytics_hit",{get:view_analytics_hit,post:view_analytics_hit})
